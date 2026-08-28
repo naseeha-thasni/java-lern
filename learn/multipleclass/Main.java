f6be557974a12762486ab91e2b437e7890d9f824
Naseeha <naseehathasnimc@gmail.com>
2026-08-28 18:36:59 +0530

    Add basic OOPs

---
+++ learn/multipleclass/Main.java
@@ -0,0 +1,6 @@
+package learn.multipleclass;
+
+public class Main {
+    int x=5;
+    
+}
---
+++ learn/multipleclass/Second.java
@@ -0,0 +1,7 @@
+class Second {
+    public static void main(String[] args) {
+        Main muobj=new Main();
+        System.out.println(muobj.x);
+    }
+
+}
